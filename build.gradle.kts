ccb18f8870fd52095cb92887919ed41cd756a748
imamyusufb <imamkotlin02@gmail.com>
2025-09-17 16:54:23 +0700

    [Fix] Adjust Splashscreen layout with onboarding screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,8 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.hiltAndroid) apply false
+    alias(libs.plugins.kotlinKsp) apply false
+
 }
 true // Needed to make the Suppress annotation work for the plugins block
